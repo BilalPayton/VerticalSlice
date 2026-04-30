8d36c3024c2a2c47b0b0107d4417f6cc9c5e8d95
BilalPayton <supersmashorange@gmail.com>
2026-04-29 18:52:42 -0700

    Added ScriptableObjects for potential items

---
+++ Totally Normal RPS/Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 1)]
+public class ItemData : ScriptableObject
+{
+    public string _name;
+    public string _description;
+    public Sprite _icon;
+}
